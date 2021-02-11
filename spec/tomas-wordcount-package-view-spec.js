dd5c0714630224c9535c23e87a5e565122b1b34a
freetrade7 <78844284+freetrade7@users.noreply.github.com>
2021-02-11 12:34:56 +0700

    Initial commit

---
+++ spec/tomas-wordcount-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TomasWordcountPackageView from '../lib/tomas-wordcount-package-view';
+
+describe('TomasWordcountPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
